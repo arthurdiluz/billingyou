eccd921171f2aaf5cb3b17b943f50539ee8e256f
Arthur Diniz <arthurdiluz@outlook.com>
2022-12-11 16:36:21 -0300

    feat(web): add customer service api

---
+++ web/src/services/CustomerService.tsx
@@ -0,0 +1,30 @@
+import { ICustomer } from "../interfaces";
+import { Api } from "../providers/Api";
+
+function create(data: Omit<ICustomer, "id">) {
+  return Api.post<ICustomer>("/api/customers", data);
+}
+
+function find() {
+  return Api.get<ICustomer[]>("/api/customers");
+}
+
+function findById(id: string) {
+  return Api.get<ICustomer>(`/api/customers/${id}`);
+}
+
+function update(id: string, data: Omit<ICustomer, "id">) {
+  return Api.patch<ICustomer>(`/api/customers/${id}`, data);
+}
+
+function deleteById(id: string) {
+  return Api.delete<void>(`/api/customers/${id}`);
+}
+
+export const CustomerService = {
+  create,
+  find,
+  findById,
+  update,
+  deleteById,
+};
