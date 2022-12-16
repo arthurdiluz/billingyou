0d32d52bef712839c6fc580b278e3c689f5d5010
Arthur Diniz <arthurdiluz@outlook.com>
2022-12-16 15:32:53 -0300

    feat(web): add form components
    input; input currency; select

--- web/src/components/Form/Input.tsx
+++ web/src/components/Form/Input.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import InputErrorMessage from "./InputErrorMessage";
+import InputErrorMessage from "@components/Form/InputErrorMessage";
 
 type Props = React.DetailedHTMLProps<
   React.InputHTMLAttributes<HTMLInputElement>,
