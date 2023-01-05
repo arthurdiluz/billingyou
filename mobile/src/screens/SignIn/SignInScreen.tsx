e323ac0eac727c7fd72f3283707d0fbb41722930
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2023-01-05 10:47:29 -0300

    feat(mobile): add SignIn screen

--- mobile/src/screens/SignIn/SignInScreen.tsx
+++ mobile/src/screens/SignIn/SignInScreen.tsx
@@ -1,9 +1,89 @@
-import { Text, View } from "react-native";
+import { StyleSheet, View, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useForm, Controller } from "react-hook-form";
+import { SignInResolver } from "../../validations/SignIn";
+import { useAuthContext } from "../../contexts/AuthContext";
+import { Title } from "../../components/Title/Title";
+import { TextInput } from "../../components/Form/TextInput";
+import { Button } from "../../components/Form/Button";
+import { BillingYouLogo } from "../../components/Icons/BillingYouLogo";
 
-export default function SignInScreen() {
+type Form = {
+  email: string;
+  password: string;
+};
+
+export function SignInScreen() {
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<Form>({ resolver: SignInResolver });
+  const { signIn } = useAuthContext();
+
+  async function onSubmit(values: Form) {
+    await signIn(values.email, values.password);
+  }
+
   return (
-    <View>
-      <Text>SignIn screen</Text>
+    <View style={styles.container}>
+      <SafeAreaView edges={["top", "left", "right"]} style={styles.safeArea}>
+        <ScrollView style={styles.content}>
+          <View style={styles.logoContainer}>
+            <BillingYouLogo width={180} />
+          </View>
+          <View style={{ marginBottom: 30 }}>
+            <Title>Access your account</Title>
+          </View>
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, ...field } }) => (
+              <TextInput
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                errors={errors}
+                {...field}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange, ...field } }) => (
+              <TextInput
+                placeholder="Password"
+                secureTextEntry
+                onChangeText={onChange}
+                errors={errors}
+                {...field}
+              />
+            )}
+          />
+
+          <Button label="Sign In" onPress={handleSubmit(onSubmit)} />
+        </ScrollView>
+      </SafeAreaView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  safeArea: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#fff",
+  },
+  logoContainer: {
+    width: "100%",
+    paddingVertical: 20,
+  },
+  content: {
+    padding: 20,
+  },
+});
