b928f39dbf9b725b6260d258949e419b19a18db0
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2023-01-06 19:43:26 -0300

    feat(mobile): add home screen

--- mobile/src/navigations/UnauthenticatedNavigation.tsx
+++ mobile/src/navigations/UnauthenticatedNavigation.tsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AboutScreen } from "../screens/About/AboutScreen";
+import { HomeScreen } from "../screens/Home/HomeScreen";
 import { SignInScreen } from "../screens/SignIn/SignInScreen";
 import { SignUpScreen } from "../screens/SignUp/SignUpScreen";
 
@@ -8,11 +9,26 @@ const Stack = createNativeStackNavigator();
 export function UnauthenticatedNavigation() {
   return (
     <>
-      <Stack.Navigator initialRouteName="SignIn">
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen
           name="SignIn"
           component={SignInScreen}
-          options={{ headerShown: false }}
+          options={{ headerShown: true }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
+          options={{ headerShown: true }}
+        />
+        <Stack.Screen
+          name="About"
+          component={AboutScreen}
+          options={{ headerShown: true }}
         />
       </Stack.Navigator>
     </>
